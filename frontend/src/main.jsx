4e41f61f58c06466ee9ad8a371b3410f2597a0ce
Mostafa Magdy <magdy.m@zenithweave.com>
2026-01-19 05:08:55 +0200

    Force frontend rebuild: Add build timestamp

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
+// Build timestamp: 2026-01-19 05:08:00
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
